1b3738b4df7c08de90f647ba257dbab28eb20f12
harsh basoya <harshbasoya@harshs-MacBook.local>
2026-05-07 12:36:11 +0530

    fixed capital letter typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from './supabaseClient';
+import { supabase } from './supabaseclient';
 import Login from './Login';
 import Paywall from './Paywall';
 
